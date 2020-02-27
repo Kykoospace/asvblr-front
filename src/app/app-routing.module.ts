7189cad57b2d036a87bbc476b4ff92081234b575
Kyllian <kyllian.gt@hotmail.fr>
2020-02-27 20:22:44 +0100

    Routes system initialisation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,29 @@
+// Angular imports :
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// Components import :
+import { MainComponent } from './main/main.component';
+import { HomeComponent } from './main/home/home.component';
+import { ManagementComponent } from './management/management.component';
+import { StatsComponent } from './management/stats/stats.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: 'main', pathMatch: 'full' },
+  { path: 'main', component: MainComponent,
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent }
+    ]
+  },
+  { path: 'management', component: ManagementComponent,
+    children: [
+      { path: '', redirectTo: 'stats', pathMatch: 'full' },
+      { path: 'stats', component: StatsComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './main/home/home.component';
+import { StatsComponent } from './management/stats/stats.component';
+import { MainComponent } from './main/main.component';
+import { ManagementComponent } from './management/management.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    StatsComponent,
+    MainComponent,
+    ManagementComponent
   ],
   imports: [
     BrowserModule,
