81e8d3dda135d2b3201c414aa18522fd4d2cc0d7
Kyllian <kyllian.gt@hotmail.fr>
2020-02-28 18:48:18 +0100

    Ajout des dépendances de primeng

--- src/app/management/management.component.ts
+++ src/app/management/management.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManagementComponent implements OnInit {
 
-  private navItems = [
+  public navItems = [
     {
       name: 'Site web',
       content: [
