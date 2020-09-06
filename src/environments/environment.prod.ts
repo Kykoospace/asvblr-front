4483e265c09721f80910e5c97c64618a98d50b27
Kyllian <kyllian.gt@hotmail.fr>
2020-09-06 22:22:44 +0200

    Retrait de la variable d'env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,3 @@
 export const environment = {
-  production: true,
-  md5_secret_key: 'b1iei6fvkn3624e0jdpr'
+  production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  md5_secret_key: 'b1iei6fvkn3624e0jdpr'
+  production: false
 };
 
 /*
